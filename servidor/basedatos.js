3232229b059fe42f4bce7ecc01ebc7db67016ae6
ale <alemarchena@gmail.com>
2019-04-02 23:18:05 -0300

    cambie el nombre de la base y su coleccion

--- servidor/basedatos.js
+++ servidor/basedatos.js
@@ -6,7 +6,7 @@ const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://alemarchena:<password>@cluster0-ryhy7.mongodb.net/test?retryWrites=true";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
-    const collection = client.db("test").collection("devices");
+    const collection = client.db("dbbanuncios").collection("coleccionanuncios");
     // perform actions on the collection object
     client.close();
 });
