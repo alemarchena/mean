b36aa9579304cc0820c245f5bcba1f7095d6cc31
ale <alemarchena@gmail.com>
2019-04-02 23:12:34 -0300

    pegue el codigo de mlab

--- servidor/basedatos.js
+++ servidor/basedatos.js
@@ -1,12 +1,23 @@
 //configuracion de la base de datos en mogodb
 const mongoose = require('mongoose');
 
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://alemarchena:<password>@cluster0-ryhy7.mongodb.net/test?retryWrites=true";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
+/*
 const config = {
     autoIndex: false,
     uri: 'mongodb + srv://alemarchena:<Picapiedra2887>@cluster0-ryhy7.mongodb.net/test?retryWrites=true',
     opt:{useNewUrlParser:true}
 };
-
+*/
 
 mongoose.connect(config.uri, config.opt)
     .then(db => console.log('Base de datos conectada'))
