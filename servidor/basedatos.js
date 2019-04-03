b65728a2ffa309a00de5959ce158d1b89372e01f
ale <alemarchena@gmail.com>
2019-04-02 23:39:34 -0300

    no anda

--- servidor/basedatos.js
+++ servidor/basedatos.js
@@ -17,12 +17,12 @@ const config = {
     uri: 'mongodb + srv://alemarchena:<Picapiedra2887>@cluster0-ryhy7.mongodb.net/test?retryWrites=true',
     opt:{useNewUrlParser:true}
 };
-*/
+
 
 mongoose.connect(config.uri, config.opt)
     .then(db => console.log('Base de datos conectada'))
     .catch(err => console.err(err));
             
-
+*/
 
 module.exports = mongoose;
