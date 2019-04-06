c00ec2e65bc592b80f67a057a53e5e428ce76830
ale <alemarchena@gmail.com>
2019-04-06 12:26:22 -0300

    Utilizando mongoose en vez de mongodb client

--- servidor/basedatos.js
+++ servidor/basedatos.js
@@ -1,20 +1,9 @@
 //configuracion de la base de datos en mogodb
 const mongoose = require('mongoose');
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://alemarchena:Picapiedra2887@cluster0-ndsgy.mongodb.net/anunciosbdd?retryWrites=true";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-    const collection = client.db("anunciosbdd").collection("anuncioscol");
-    // perform actions on the collection object
-    client.close();
-});
-
-
-/*
 const config = {
     autoIndex: false,
-    uri: 'mongodb + srv://alemarchena:<Picapiedra2887>@cluster0-ryhy7.mongodb.net/test?retryWrites=true',
+    uri: 'mongodb+srv://alemarchena:Picapiedra2887@cluster0-ndsgy.mongodb.net/anunciosdbb?retryWrites=true',
     opt:{useNewUrlParser:true}
 };
 
@@ -22,7 +11,6 @@ const config = {
 mongoose.connect(config.uri, config.opt)
     .then(db => console.log('Base de datos conectada'))
     .catch(err => console.err(err));
-            
-*/
+
 
 module.exports = mongoose;
