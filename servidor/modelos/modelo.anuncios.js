0d28d56d443e48b1a95d3fc743d8220d278f7bb6
ale <alemarchena@gmail.com>
2019-04-06 20:19:42 -0300

    modifique el nombre del esquema

--- servidor/modelos/modelo.anuncios.js
+++ servidor/modelos/modelo.anuncios.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const {Schema} = mongoose; //no necesito todo mongoose, entonces solo armo el esquema
 
-const ModeloAnuncios = new Schema({
+const anuncioscol = new Schema({
     nombre:     {type: String,required:true},
     titulo:     {type: String,required:true},
     descripcion:{ type:String,required:true},
@@ -10,4 +10,4 @@ const ModeloAnuncios = new Schema({
     imagen:     {type: String,required:true}
 });
 
-module.exports = mongoose.model('ModeloAnuncios', ModeloAnuncios); //le digo a mongodb como se va a llamar el esquema y cual es su tipo
+module.exports = mongoose.model('anuncioscol', anuncioscol); //le digo a mongodb como se va a llamar el esquema y cual es su tipo
