09c6869664892f848fb092f1c5ab5e7765d04078
ale <alemarchena@gmail.com>
2019-04-06 20:47:24 -0300

    armando de nuevo la coneccion

--- servidor/basedatos.js
+++ servidor/basedatos.js
@@ -1,7 +1,7 @@
 //configuracion de la base de datos en mogodb
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://alejandro:hVcBbEzFe4l4vdlG@cluster0-ryhy7.mongodb.net/anunciosbdd?retryWrites=true', { useNewUrlParser: true })
+mongoose.connect('mongodb+srv://alejandro:hVcBbEzFe4l4vdlG@cluster0-ndsgy.mongodb.net/test?retryWrites=true', { useNewUrlParser: true })
 const db = mongoose.connection
 
 db.on('error', error => console.error(error))
