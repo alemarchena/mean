4d8779d8c71c2d63b2be80cec2eda4216bd57e84
ale <alemarchena@gmail.com>
2019-05-09 21:55:02 -0300

    Configuracion de CORS en el servidor

--- servidor/index.js
+++ servidor/index.js
@@ -12,6 +12,9 @@ const cors = require('cors');
 
 const { mongoose } = require('./basedatos'); //aqui tengo la conexion de mongoose a la base de datos
 const app = express(); //aqui toma toda la funcionalidad del servidor  y la coloca en la variable app
+app.use(cors());
+
+
 const expressLayouts = require('express-ejs-layouts')
 const rutas = require('./rutas/anuncios.rutas');
 
@@ -24,7 +27,17 @@ app.use(express.json()); //los datos que llegan del navegador se interpretan com
 
 app.use(express.static('/servidor/public'));
 app.use('/', rutas);
-app.use(cors({ origin:'https://mean-templay.herokuapp.com/'}));
+
+
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 
 app.listen(app.get('puerto'), () => {
     const puertoconectado = app.get('puerto');
