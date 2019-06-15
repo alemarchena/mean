caf7fa94cb7e8d33f44b2136e0c43056183d91b3
ale <alemarchena@gmail.com>
2019-06-15 14:39:23 -0300

    Separando servidores

---
+++ servidorempresas/controlador/empresa.controlador.js
@@ -0,0 +1,53 @@
+//va a tener las funciones para trabajar con la base de datos
+const ModeloEmpresa = require('../modelos/modelo.empresas');
+
+
+const controladorEmpresa = {};
+
+
+controladorEmpresa.leerEmpresas = async (req, res) => {
+    const empresas = await ModeloEmpresa.find();
+    res.json(empresas);
+}
+
+controladorEmpresa.leerEmpresa = async (req, res) => {
+
+    const empresa = await ModeloEmpresa.findById(req.params.id);
+    res.json(empresa);
+}
+
+controladorEmpresa.crearEmpresa = async (req, res) => {
+    const empresa = new ModeloEmpresa({
+        idpanel: req.body.idpanel,
+        idmapa: req.body.idmapa,
+        nombreempresa: req.body.nombreempresa,
+        imagen: req.body.imagen
+    });
+    await empresa.save();
+    res.json({
+        'estado': 'Empresa guardada'
+    });
+}
+
+controladorEmpresa.actualizarEmpresa = async (req, res) => {
+    const { id } = req.params; //le digo dame id que este dentro de params
+    const empresa = {
+        idpanel: req.body.idpanel,
+        idmapa: req.body.idmapa,
+        nombreempresa: req.body.nombreempresa,
+        imagen: req.body.imagen
+    }
+    await ModeloEmpresa.findByIdAndUpdate(id, { $set: empresa }, { new: true }); //le digo que busco por id y que datos quiero actualizar, con new : true si no existe lo crea
+    res.json({
+        status: "Empresa actualizada"
+    });
+}
+
+
+controladorEmpresa.borrarEmpresa = async (req, res) => {
+    const { id } = req.params;
+    await ModeloEmpresa.findByIdAndRemove(id);
+    res.json({ status: 'Empresa eliminada' });
+}
+
+module.exports = controladorEmpresa;
---
+++ servidorempresas/index.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+
+//valida la variable del servidor en produccion
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').parse('DATABASE_URL');
+}
+
+//requerimientos
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const { mongoose } = require('./basedatos'); //aqui tengo la conexion de mongoose a la base de datos
+const app = express(); //aqui toma toda la funcionalidad del servidor  y la coloca en la variable app
+app.use(cors());
+
+const rutas = require('./rutas/empresas.rutas');
+
+//configuraciones
+app.set('puerto', process.env.PORT || 3000)
+
+//objetos
+app.use(morgan('start')); //las peticiones pasan por MORGAN y devuelven info tal como un error.
+app.use(express.json()); //los datos que llegan del navegador se interpretan como json
+
+app.use('/', rutas);
+
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+
+app.listen(app.get('puerto'), () => {
+    const puertoconectado = app.get('puerto');
+    console.log("Servidor empresas iniciado en puerto " + puertoconectado);
+});
