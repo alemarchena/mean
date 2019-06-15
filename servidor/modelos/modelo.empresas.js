236feeafdd6bd93edf274687efef94dbdbb8b451
ale <alemarchena@gmail.com>
2019-06-15 14:10:18 -0300

    Arreglando

--- servidor/modelos/modelo.empresas.js
+++ servidor/modelos/modelo.empresas.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
+const mongooseE = require('mongoose');
 
-const { Schema } = mongoose; //no necesito todo mongoose, entonces solo armo el esquema
+const { SchemaE } = mongoose; //no necesito todo mongoose, entonces solo armo el esquema
 
-const empresascol = new Schema({
+const empresascol = new SchemaE({
     idpanel: { type: Number},
     idmapa: { type: Number },
     nombreempresa: { type: String, required: true },
     imagen: { type: String, required: true }
 });
 
-module.exports = mongoose.model('coleccionempresas', empresascol); //le digo a mongodb como se va a llamar el esquema y cual es su tipo
+module.exports = mongooseE.model('coleccionempresas', empresascol); //le digo a mongodb como se va a llamar el esquema y cual es su tipo
